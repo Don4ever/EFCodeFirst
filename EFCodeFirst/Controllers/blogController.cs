538fb3e261406a64f1ce9b67ee2afd812361b542
Don4ever <qassem.albahdour@gmail.com>
2017-11-06 19:08:03 +0400

    After adding blog "controller & view"

---
+++ EFCodeFirst/Controllers/blogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EFCodeFirst.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFCodeFirst.Controllers
+{
+    public class blogController : Controller
+    {
+        // GET: blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            return View();
+        }
+    }
+}
